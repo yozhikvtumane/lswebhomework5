2bb7a759d48a0435b93c34f3d7be2c0367a76cef
vasya <toshamatveeev@gmail.com>
2017-06-03 13:37:50 +0200

    Homework 5

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
 //1. print the number 5 to the console
+console.log("1. Print the number 5 to the console:");
 console.log(5);
+
 //2. Print your name to the console
+console.log("2. Print your name to the console:");
 console.log("John");
+
 //3. Store your age as a variable called "myAge"
 var myAge = 10;
+
 //4. Print to the console how old you will be in 5 years
+console.log("4. Print to the console how old you will be in 5 years");
 console.log(myAge + 5);
+
 //5. Store your favorite food as a variable called "myFavoriteFood"
 var myFavoriteFood = "Burgers";
+
 //6. Publish your favorite food to `index.html` using `document.write()`
 document.write(myFavoriteFood);
+
 //7. Print the remainder of 14 / 3 to the console
+console.log("Print the remainder of 14 / 3 to the console");
 console.log(14 % 3);
+
 //8. Print the remainder of 829 / 13 to the console
+console.log("Print the remainder of 829 / 13 to the console");
 console.log(829 % 13);
+
 //9. Create a for loop that counts from 0 to 130 by 3s
+console.log("Create a for loop that counts from 0 to 130 by 3s");
 for (var i = 0; i <= 130; i += 3) {
 	console.log(i);
 }
+
 //10. Create a for loop that counts from 3 to 17 by 2s
+console.log("Create a for loop that counts from 3 to 17 by 2s");
 for (var i = 3; i <= 17; i += 2) {
 	console.log(i);
 }
+
 //11. Create a for loop that counts from 100 to 3 by -1
+console.log("Create a for loop that counts from 100 to 3 by -1");
 for (var i = 100; i >= 3 ; i--) {
 	console.log(i);
 }
+
 //12. Create a for loop that counts from 1 to 100 by 1s
+console.log("Create a for loop that counts from 1 to 100 by 1s");
 for (var i = 1; i <=100 ; i++) {
 	console.log(i);
 }
+
 //13. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizz` if the number is divisible by 5
-console.log("Output Fizz if i % 5 == 0");
+console.log("Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizz` if the number is divisible by 5");
 for (var i = 1; i <= 100; i++) {
 	if (i % 5 == 0) {
 		console.log("fizz");
@@ -39,8 +60,9 @@ for (var i = 1; i <= 100; i++) {
 		console.log(i);
 	}
 }
+
 //14. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `buzz` if the number is divisible by 3
-console.log("Output Buzz if i % 3 == 0");
+console.log("Create a for loop that counts from 1 to 100, but instead of printing `i` prints `buzz` if the number is divisible by 3");
 for (var i = 1; i <= 100; i++) {
 	if (i % 3 == 0) {
 		console.log("buzz");
@@ -48,7 +70,9 @@ for (var i = 1; i <= 100; i++) {
 		console.log(i);
 	}
 }
+
 //15. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizzbuzz` if the number is divisible by 15
+console.log("Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizzbuzz` if the number is divisible by 15");
 console.log("Output FizzBuzz if i % 15 == 0");
 for (var i = 1; i <= 100; i++) {
 	if (i % 15 == 0) {
@@ -69,8 +93,7 @@ The "Fizz-Buzz test" is an interview question designed to help filter out the 99
 Hint: Use your last three loops and if/then/else statements. You can learn about those here https://www.w3schools.com/js/js_if_else.asp
 
 */
-console.log("Extra:");
-
+console.log("Extra: FizzBuzz");
 for (var i = 1; i <= 100; i++) {
 	if (i % 3 == 0) {
 		if (i % 5 == 0) {
